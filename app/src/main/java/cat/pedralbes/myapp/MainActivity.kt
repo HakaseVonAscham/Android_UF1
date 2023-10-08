cf809fc12d090de0be380126c86fa09e5c5cc78e
Luis Axel Godoy Vela <a22luigodvel@inspedralbes.cat>
2023-10-08 22:07:13 +0200

    Ejercicio8

--- app/src/main/java/cat/pedralbes/myapp/MainActivity.kt
+++ app/src/main/java/cat/pedralbes/myapp/MainActivity.kt
@@ -4,16 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 abstract class MainActivity : AppCompatActivity() {
     private lateinit var button: Button
+    private lateinit var text: EditText
+    private lateinit var viewer: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button = findViewById<Button>(R.id.loginButton)
+        button = findViewById<Button>(R.id.textButton)
+        text = findViewById(R.id.textInput)
+        viewer = findViewById(R.id.textViewer)
     }
 
     fun sendMeesage(view: View) {
@@ -21,5 +27,6 @@ abstract class MainActivity : AppCompatActivity() {
         val duration = Toast.LENGTH_SHORT
         val toast = Toast.makeText(this, text, duration)
         toast.show()
+        viewer.setText(text.toString())
     }
 }
