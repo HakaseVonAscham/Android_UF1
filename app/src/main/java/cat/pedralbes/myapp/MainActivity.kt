7e1800a159efd1a4f354f6ac23a5596c0d92fbe8
Luis Axel Godoy Vela <a22luigodvel@inspedralbes.cat>
2023-10-08 21:26:10 +0200

    Correciones

--- app/src/main/java/cat/pedralbes/myapp/MainActivity.kt
+++ app/src/main/java/cat/pedralbes/myapp/MainActivity.kt
@@ -2,24 +2,17 @@ package cat.pedralbes.myapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.Toast
 
 abstract class MainActivity : AppCompatActivity() {
-    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        button = findViewById<Button>(R.id.button)
     }
 
     fun sendMeesage(view: View) {
-        val text = "Button pressed!"
-        val duration = Toast.LENGTH_SHORT
-        val toast = Toast.makeText(this, text, duration)
-        toast.show()
+        Log.i(String(),"Button pressed")
     }
 }
