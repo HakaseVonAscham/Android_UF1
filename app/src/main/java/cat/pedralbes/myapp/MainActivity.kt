97a9a7b139592962e902a5e43c8e0afb45bc05b2
Luis Axel Godoy Vela <axellvg@outlook.es>
2023-10-11 17:08:18 +0200

    Corrections

--- app/src/main/java/cat/pedralbes/myapp/MainActivity.kt
+++ app/src/main/java/cat/pedralbes/myapp/MainActivity.kt
@@ -2,12 +2,20 @@ package cat.pedralbes.myapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.view.View
 
-abstract class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
+    fun saveData(view: View) {
+        val text = "Saved"
+        val duration = Toast.LENGTH_SHORT
+        val toast = Toast.makeText(this, text, duration)
+        toast.show()
+    }
 }
