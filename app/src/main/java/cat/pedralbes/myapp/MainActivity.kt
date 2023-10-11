70deb479d48d7cc5886bed26aa675efd5d3ba630
Luis Axel Godoy Vela <axellvg@outlook.es>
2023-10-11 16:54:49 +0200

    Corrections

--- app/src/main/java/cat/pedralbes/myapp/MainActivity.kt
+++ app/src/main/java/cat/pedralbes/myapp/MainActivity.kt
@@ -6,14 +6,12 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 
-abstract class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        button = findViewById<Button>(R.id.loginButton)
     }
 
     fun sendMeesage(view: View) {
