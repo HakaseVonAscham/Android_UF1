1896f68eb2498e8cbbeaf5e8ec6978155cd4ee8b
Luis Axel Godoy Vela <axellvg@outlook.es>
2023-10-11 17:19:10 +0200

    Test1

--- app/src/main/java/cat/pedralbes/myapp/MainActivity.kt
+++ app/src/main/java/cat/pedralbes/myapp/MainActivity.kt
@@ -2,6 +2,7 @@ package cat.pedralbes.myapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import android.view.View
 
@@ -10,6 +11,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(String(),"Created")
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.i(String(),"Started")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(String(),"Resumed")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(String(),"Paused")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(String(),"Stop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(String(),"Destroied")
     }
 
     fun saveData(view: View) {
