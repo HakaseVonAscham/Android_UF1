3487ae60a587ec8c51aa6f71876295690628a94a
Luis Axel Godoy Vela <axellvg@outlook.es>
2023-10-11 16:46:22 +0200

    Correction

--- app/src/main/java/cat/pedralbes/myapp/MainActivity.kt
+++ app/src/main/java/cat/pedralbes/myapp/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 
-abstract class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
