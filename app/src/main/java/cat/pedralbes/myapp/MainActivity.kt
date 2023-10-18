898ef168370c4d02f7fec2584dd2b2d3b680d39b
Luis Axel Godoy Vela <axellvg@outlook.es>
2023-10-18 17:25:08 +0200

    Ejercicio12

--- app/src/main/java/cat/pedralbes/myapp/MainActivity.kt
+++ app/src/main/java/cat/pedralbes/myapp/MainActivity.kt
@@ -3,6 +3,9 @@ package cat.pedralbes.myapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +13,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.i(String(),"Created")
+
+        val firstname: EditText = findViewById<EditText>(R.id.Firstname)
+        val lastname: EditText = findViewById<EditText>(R.id.Lastname)
+        val age: EditText = findViewById<EditText>(R.id.Age)
+        val username: EditText = findViewById<EditText>(R.id.Username)
+        val password: EditText = findViewById<EditText>(R.id.Password)
     }
+
+    fun sendMeesage(view: View) {
+        val text = "Registred!"
+        val duration = Toast.LENGTH_SHORT
+        val toast = Toast.makeText(this, text, duration)
+        toast.show()
+    }
+
     override fun onStart() {
         super.onStart()
         Log.i(String(),"Started")
