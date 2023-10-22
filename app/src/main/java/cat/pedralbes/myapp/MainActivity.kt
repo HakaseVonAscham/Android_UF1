071e2703eb91d75e96897e447611fc52bd4e8549
Luis Axel Godoy Vela <a22luigodvel@inspedralbes.cat>
2023-10-22 23:43:25 +0200

    EJERCICIO14

--- app/src/main/java/cat/pedralbes/myapp/MainActivity.kt
+++ app/src/main/java/cat/pedralbes/myapp/MainActivity.kt
@@ -13,19 +13,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.i(String(),"Created")
-
-        val firstname: EditText = findViewById<EditText>(R.id.Firstname)
-        val lastname: EditText = findViewById<EditText>(R.id.Lastname)
-        val age: EditText = findViewById<EditText>(R.id.Age)
-        val username: EditText = findViewById<EditText>(R.id.Username)
-        val password: EditText = findViewById<EditText>(R.id.Password)
-    }
-
-    fun sendMeesage(view: View) {
-        val text = "Registred!"
-        val duration = Toast.LENGTH_SHORT
-        val toast = Toast.makeText(this, text, duration)
-        toast.show()
     }
 
     override fun onStart() {
